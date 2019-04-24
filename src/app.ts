e81a5ef0e086fe113f81f59c6a09bed58f71d50e
Artur Kula <kontakt@arturkula.com>
2019-04-24 07:51:19 +0200

    finished lesson: "A “this” Keyword Primer"

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
 // run in console with: clear; "running tsc:"; tsc; ""; "running node:"; ""; node .\dist\app.js; "";
+
+function myFunction() {
+    console.debug('Function:::', this);
+}
+
+myFunction();
+
+// object literal
+const myObj = {
+    myMethod() {
+        console.debug('Object:::', this);
+    },
+};
+
+myObj.myMethod();
+
+// Classes
+class MyClass {
+    public myMethod() {
+        console.debug('Class:::', this);
+    }
+}
+
+const myInstance = new MyClass();
+myInstance.myMethod();
