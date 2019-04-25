b7e3b23ab6f5ac7a02520f52adfa37d1e770757b
Artur Kula <kontakt@arturkula.com>
2019-04-25 07:53:30 +0200

    finished lesson: "Exploring “this” with .call, .apply and .bind"

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,22 @@
 // run in console with: clear; "running tsc:"; tsc; ""; "running node:"; ""; node .\dist\app.js; "";
 
-function myFunction() {
-    console.debug('Function:::', this);
-}
-
-myFunction();
-
-// object literal
+// Object literal
 const myObj = {
     myMethod() {
         console.debug('Object:::', this);
     },
 };
 
-myObj.myMethod();
+// myObj.myMethod();
 
-// Classes
-class MyClass {
-    public myMethod() {
-        console.debug('Class:::', this);
-    }
+// Function
+function myFunction(...text: string[]) {
+    console.debug('Function:::', this, text);
 }
 
-const myInstance = new MyClass();
-myInstance.myMethod();
+const bindFunction = myFunction.bind(myObj);
+bindFunction('abc', 'def');
+bindFunction('a1c', 'def');
+bindFunction('ab3', 'def');
+myFunction.call(myObj, 'abc', 'def');
+myFunction.apply(myObj, ['abc', 'def']);
