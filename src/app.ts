68f7f79f2a86313915ba5c6091b0d6a9cbc83342
Artur Kula <kontakt@arturkula.com>
2019-04-26 06:52:36 +0200

    finished lesson: "Arrow Functions and Lexical Scope"

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,15 @@
 // run in console with: clear; "running tsc:"; tsc; ""; "running node:"; ""; node .\dist\app.js; "";
+// run in console with: yarn start
 
-// Object literal
-const myObj = {
-    myMethod() {
-        console.debug('Object:::', this);
-    },
-};
+class MyClass {
+    public myMethod() {
+        const foo = 123;
 
-// myObj.myMethod();
+        console.debug('1', this);
 
-// Function
-function myFunction(...text: string[]) {
-    console.debug('Function:::', this, text);
+        setTimeout(() => console.debug('2', this));
+    }
 }
 
-const bindFunction = myFunction.bind(myObj);
-bindFunction('abc', 'def');
-bindFunction('a1c', 'def');
-bindFunction('ab3', 'def');
-myFunction.call(myObj, 'abc', 'def');
-myFunction.apply(myObj, ['abc', 'def']);
+const myInstance = new MyClass();
+myInstance.myMethod();
