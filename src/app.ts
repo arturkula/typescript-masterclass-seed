2b78cf0acc88c7bdf040bb63ff350e01e3c34d4a
Artur Kula <kontakt@arturkula.com>
2019-05-24 07:11:42 +0200

    finished lesson: "Literal Type Guards and "in" Operator"

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,28 @@
 // run in console with: yarn start
 
+// // introduction:
+// const exists = 'localStorage' in window; // prop exists in obj
+// const foo = 'bar'; // literal type of 'bar'
+// const foo: string = 'bar'; // string type
+
 class Song {
+    public kind: 'song';
     constructor(public title: string, public duration: number) {}
 }
 
 // tslint:disable-next-line:max-classes-per-file
 class Playlist {
+    public kind: 'playlist';
     constructor(public name: string, public songs: Song[]) {}
 }
 
 function isSong(item: any): item is Song {
-    return item instanceof Song;
+    return 'title' in item; // title prop exists in item
 }
 
 function getItemName(item: Song | Playlist): string {
-    if (isSong(item)) {
+    // if (isSong(item)) {
+    if (item.kind === 'song') {
         return item.title;
     }
     return item.name;
