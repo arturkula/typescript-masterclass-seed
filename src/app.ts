e5922a885a893da691a488e99402397410de600e
Artur Kula <kontakt@arturkula.com>
2019-07-17 08:05:18 +0200

    finished lesson: "Emitting Source Maps"

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
 // run in console with: yarn start
 // or with: clear; "running tsc:"; tsc; ""; "running node:"; ""; node .\dist\app.js; "";
 
+class Person {
+    public name: string = 'Artur';
+}
+
 console.debug('this is app file');
