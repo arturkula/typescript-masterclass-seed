0b7f63ba8632a8d44550f3fb04e150dfb1116707
Artur Kula <kontakt@arturkula.com>
2019-07-17 07:39:00 +0200

    finished lesson: "Include, Exclude and Files Properties"

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,3 @@
 // run in console with: yarn start
 
-export class Foo {
-
-    constructor(public name: string) { }
-
-    public bar(age: number) {
-
-        console.debug('age: ', age);
-    }
-}
+console.debug('This is a not test');
