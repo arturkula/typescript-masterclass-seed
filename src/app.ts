bc7accbd292e025d36bb1a6058837c7d7d9d2096
Artur Kula <kontakt@arturkula.com>
2019-07-18 08:11:47 +0200

    finished lesson: "CLI Output Tweaks"

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 // run in console with: yarn start
 // or with: clear; "running tsc:"; tsc; ""; "running node:"; ""; node .\dist\app.js; "";
 
-class Person {
-    public name: string;
-}
+class Person { }
+
 console.debug('this is app file');
