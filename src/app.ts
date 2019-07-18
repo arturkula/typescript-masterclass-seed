aa6dcde4d28afdd50ba2403ed35e00d4c943a621
Artur Kula <kontakt@arturkula.com>
2019-07-18 07:59:26 +0200

    finished lesson: "Understanding "lib" and ES libraries"

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
 // run in console with: yarn start
 // or with: clear; "running tsc:"; tsc; ""; "running node:"; ""; node .\dist\app.js; "";
 
-export class Person {
-
-    constructor(private name: string) { }
-
-    public getName() {
-        return this.name;
-    }
-}
+Array.from(document.querySelectorAll('*'));
 
 console.debug('this is app file');
