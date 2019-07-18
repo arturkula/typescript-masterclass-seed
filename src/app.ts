d54667d52e7d1830dc90336e9d1e8f85c7a8df0a
Artur Kula <kontakt@arturkula.com>
2019-07-18 07:50:42 +0200

    finished lesson: "Transpiling and Experimental Features"

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 // run in console with: yarn start
 // or with: clear; "running tsc:"; tsc; ""; "running node:"; ""; node .\dist\app.js; "";
 
-class Person {
-    public name: string = 'Artur';
+export class Person {
+
+    constructor(private name: string) { }
+
+    public getName() {
+        return this.name;
+    }
 }
 
 console.debug('this is app file');
